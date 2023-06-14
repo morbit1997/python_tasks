d45f97e42c1342602e15a9460f33cc77fd31d979
morbit1997 <morbit1997@gmail.com>
2023-06-14 11:09:45 +0000

    сделаны заданяи до task_6_2b

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,38 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+flag = 0
+while flag != 1:
+   flag = 0
+   ip = input('Введите IP-адрес в формате 10.0.1.1: ')
+   ip_split = ip.split('.')
+   if len(ip_split) == 4:
+      for elem in ip_split:
+         if elem.isdigit():
+            if 0 <= int(elem) <= 255:
+               flag = 1
+               continue
+            else:
+               print('Неправильный IP-адрес')
+               flag = 0
+               break
+         else:
+            print('Неправильный IP-адрес')
+            flag = 0
+            break
+   elif ip.count('.') != 3:
+      print('Неправильный IP-адрес')
+      flag = 0
+   if flag == 1:
+      if 1 <= int(ip_split[0]) <= 223:
+         print('unicast')
+      elif 224 <= int(ip_split[0]) <= 239:
+         print('multicast')
+      elif ip == "255.255.255.255":
+         print('local broadcast')
+      elif ip == "0.0.0.0":
+         print('unassigned')
+      else:
+         print('unused')
+   else:
+      break
